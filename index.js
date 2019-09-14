a482439ef34f5df0f621d6e3379c4007670963ce
Karl Lew <karl@firepick.org>
2019-09-14 16:15:28 -0700

    guid-store

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 module.exports = {
     Asset: require('./src/asset'),
+    GuidStore: require('./src/guid-store'),
     Observation: require('./src/observation'),
     Scanner: require('./src/scanner'),
 
